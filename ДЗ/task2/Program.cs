221b6f736745af6b9321042d5681f30596c063d1
nastya <nastik3344@yandex.by>
2022-12-06 22:52:27 +0300

    variant

---
+++ ДЗ/task2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите первое число 1: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число 2: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if ((number1 > number2))
+{
+    Console.WriteLine("Число 1 является max");
+}
+else
+{
+    Console.WriteLine("Число 2 является max");
+}
